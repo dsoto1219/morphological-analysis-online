933e080838735e33b84ffe151379aa036f9d71d2
thatonemailbox <127370142+thatonemailbox@users.noreply.github.com>
2025-04-15 12:35:35 -0400

    move langInfo object into separate file

--- script.js
+++ script.js
@@ -3,39 +3,6 @@ let hfst;
 let lang;
 let transducer;
 
-const langInfo = {
-    "french": {
-        "file" : "french.hfst.ol",
-        "placeholder" : "Entrez un mot ou une phrase à analyser",
-        "empty-input" : "Veuillez saisir un mot ou une phrase à analyser"
-    },
-    "english": {
-        "file" : "english.hfstol",
-        "placeholder" : "Enter a word or sentence to analyze",
-        "empty-input" : "Please enter a word or sentence to analyze"
-    },
-    "italian": {
-        "file" : "italian.hfst.ol",
-        "placeholder" : "Inserisci una parola o una frase da analizzare",
-        "empty-input" : "Inserisci una parola o una frase da analizzare" 
-    },
-    "german": {
-        "file" : "german.hfst.ol",
-        "placeholder" : "Geben Sie ein Wort oder einen Satz zur Analyse ein",
-        "empty-input" : "Bitte geben Sie ein Wort oder einen Satz zur Analyse ein" 
-    },
-    "swedish": {
-        "file" : "swedish.hfstol",
-        "placeholder" : "Ange ett ord eller en mening att analysera",
-        "empty-input" : "Vänligen ange ett ord eller en mening att analysera"
-    },
-    "finnish": {
-        "file" : "finnish-analysis.hfst",
-        "placeholder" : "Kirjoita analysoitava sana tai lause",
-        "empty-input" : "Anna analysoitava sana tai lause"
-    },
-};
-
 const langSelect = document.querySelector('.langSelect');
 const analyzeBtn = document.querySelector('.analyzeButton')
 const textInput = document.querySelector('.textInput')
